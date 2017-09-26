d3c89687c0abf9271c7bde59b47f7e625de1ff39
Maycon D <maycout2@gmail.com>
2017-09-26 11:48:39 -0300

    Cena1 - O Comeco

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+	public void LoadScene(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+}
