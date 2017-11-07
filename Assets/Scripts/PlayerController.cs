dd3d51a8f50f3c1bd381926e8522f9bea19e254e
Maycon D <maycout2@gmail.com>
2017-11-07 10:07:26 -0200

    Correcao:TesteDeMueda no Player MANO

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -127,4 +127,10 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Coin")) {
+				Destroy(other.gameObject);
+		}
+	}
+
 }
